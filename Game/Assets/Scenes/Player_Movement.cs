3ef1d0539d14a623b72e56fccf950725effcb6a4
Karl Olsen <olsenk2@rpi.edu>
2014-08-31 14:10:49 -0400

    Player movement + starting working on collisions (in Karl_Scene)

---
+++ Game/Assets/Scenes/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+
+	public float speed = 20.0f;
+	
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.W))	{
+			transform.position += Vector3.forward * Time.deltaTime;
+		}
+		if(Input.GetKey (KeyCode.A))	{
+			transform.position += Vector3.left * Time.deltaTime;
+		}
+		if(Input.GetKey (KeyCode.S))	{
+			transform.position += Vector3.back * Time.deltaTime;
+		}
+		if(Input.GetKey (KeyCode.D))	{
+			transform.position += Vector3.right * Time.deltaTime;
+		}
+	}
+
+}
