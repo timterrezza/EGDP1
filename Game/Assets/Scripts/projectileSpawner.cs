56d50bae7a088092b4625db594ab67850caba5b0
timterrezza <terret@rpi.edu>
2014-09-03 15:13:51 -0400

    changed spawning system, made ideas not able to collide with each other

--- Game/Assets/Scripts/projectileSpawner.cs
+++ Game/Assets/Scripts/projectileSpawner.cs
@@ -32,46 +32,41 @@ public class projectileSpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timeObject = GameObject.Find("Timer").GetComponent<Timer_Code>().time_limit;
-		if (timeObject > 40) {
-			timeBetweenSpawns = 4f;
-			teacherTimeBetweenSpawns = 4.5f;
+		if (timeObject > 45) {
+			timeBetweenSpawns = 2f;
+			teacherTimeBetweenSpawns = 5f;
 		}
-		else if (timeObject > 30) {
-			timeBetweenSpawns = 3f;
-			teacherTimeBetweenSpawns = 3.5f;
+		else if (timeObject > 34) {
+			timeBetweenSpawns = 1.5f;
+			teacherTimeBetweenSpawns = 4f;
 		}
-		else if (timeObject > 20) {
-			timeBetweenSpawns = 2.5f;
+		else if (timeObject > 26) {
+			timeBetweenSpawns = 2f;
 			teacherTimeBetweenSpawns = 3f;
 		}
-		else if (timeObject > 15) {
-			timeBetweenSpawns = 1f;
-			teacherTimeBetweenSpawns = 1f;
+		else if (timeObject > 16) {
+			timeBetweenSpawns = 0.8f;
+			teacherTimeBetweenSpawns = 4f;
 		}
-		else if (timeObject > 8) {
-			timeBetweenSpawns = 2.5f;
+		else if (timeObject > 10) {
+			timeBetweenSpawns = 1.5f;
 			teacherTimeBetweenSpawns = 3f;
 		}
 		else {
-			timeBetweenSpawns = 1f;
-			teacherTimeBetweenSpawns = 1f;
+			timeBetweenSpawns = 0.8f;
+			teacherTimeBetweenSpawns = 4f;
 		}
 		if (!gameOver) {
 			timer += Time.deltaTime;
 			teacherTimer += Time.deltaTime;
 			if (timer > timeBetweenSpawns) {
 				int spawnIndex = Random.Range(0, spawnPoints.Length);
-				if (spawnPoints[spawnIndex].name == "teacher") {
+				int goodChance = Random.Range (0, 20);
+				if (goodChance == 0) {
 					projectile = Resources.Load("Prefabs/goodProjectile");
 				}
 				else {
-					int goodChance = Random.Range (0, 5);
-					if (goodChance == 0) {
-						projectile = Resources.Load("Prefabs/goodProjectile");
-					}
-					else {
-						projectile = Resources.Load("Prefabs/badProjectile");
-					}
+					projectile = Resources.Load("Prefabs/badProjectile");
 				}
 				Instantiate(projectile, new Vector3(spawnPoints[spawnIndex].gameObject.transform.localPosition.x, spawnPoints[spawnIndex].gameObject.transform.localPosition.y,-0.1f),Quaternion.identity);
 				timer = 0;
